8b67f3ec1859d51b69216cda7166b6f45fdcf635
Ruan Ferreira <droderuan@hotmail.com>
2020-11-17 00:17:03 -0300

    Added a Custom class Error

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 import { errors } from 'celebrate';
 
 // mongoose connection
 import './config/mongoose';
 
+import AppError from 'errors/AppError';
 import routes from './routes';
 
 const app = express();
@@ -14,6 +15,20 @@ app.use(express.json());
 app.use(routes);
 
 app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('👽 server started');
