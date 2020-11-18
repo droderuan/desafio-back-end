98303df349413f3819933fce7f5e5304a59221a2
Ruan Ferreira <droderuan@hotmail.com>
2020-11-18 17:06:19 -0300

    - Add cors
    - Add babel to build project

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 import { errors } from 'celebrate';
+import cors from 'cors';
 
 // mongoose connection
 import './config/mongoose';
 
-import AppError from 'errors/AppError';
+import AppError from './errors/AppError';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
